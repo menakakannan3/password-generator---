daf6c4d5fe54b17f4edb135070df39b3bc358a76
menakakannan3 <86248020+menakakannan3@users.noreply.github.com>
2022-03-05 11:54:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var password=document.getElementById("password");
+document.getElementById("buttongen").addEventListener('click', genPassword);
+
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+document.getElementById("copybtn").addEventListener('click',copypassword)
+function copypassword(){
+   var copytext = document.getElementById("password");
+   copytext.select();
+   navigator.clipboard.writeText(copytext.value);
+}
